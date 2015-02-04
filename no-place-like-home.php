610bfad488f8e58b440fb93622df9d99f02d0821
sillybean <sillybean@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-04 02:39:00 +0000

    1.4: updated to use Dashicons; removed support for WP 2.3-2.7
    git-svn-id: https://plugins.svn.wordpress.org/no-place-like-home/tags/1.4@1081962 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ no-place-like-home.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: No Place Like Home
+Plugin URI: http://sillybean.net/code/no-place-like-home/
+Description: Displays a small home icon next to the designated home page under <a href="edit-pages.php">Manage &rarr; Pages</a>.
+Author: Stephanie Leary
+Version: 1.4
+Author URI: http://sillybean.net/
+*/ 
+
+
+function no_place_like_home_css() {
+	if ( get_option( 'show_on_front' ) !== 'page' ) 
+		return;
+		
+	$pageid = get_option('page_on_front');
+	
+	echo '<style type="text/css">';
+	printf( 'tr#page-%d td.page-title strong:before { 
+		content: "\f102";
+		display: inline-block;
+		-webkit-font-smoothing: antialiased;
+		font: normal 20px/1 "dashicons";
+		vertical-align: top; }', $pageid );
+	echo '</style>';
+}
+
+add_action( 'admin_head', 'no_place_like_home_css' );
